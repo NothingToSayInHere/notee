5dec57c2afb0db3b33e4fa5913ba3421e583b6e0
NothingToSayInHere <adrianus_20@yahoo.com>
2023-04-09 19:19:22 +0200

    repaired mvvm, can save note in DB. its tested (Adrian)

--- app/src/main/java/com/example/notee/NoteRepository.java
+++ app/src/main/java/com/example/notee/NoteRepository.java
@@ -1,21 +1,43 @@
 package com.example.notee;
 
 import android.app.Application;
+import android.content.Context;
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
 import com.example.notee.model.Note;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class NoteRepository {
 
     private NoteDatabaseHelper databaseHelper;
+    private ExecutorService executor;
 
-    public NoteRepository(Application context) {
+
+    public NoteRepository(Context context) {
         Log.d("NoteRepository", "Creating new instance of NoteDatabaseHelper in constructor");
         databaseHelper = new NoteDatabaseHelper(context);
+        executor = Executors.newSingleThreadExecutor();
     }
 
     public void addNoteToDatabase(Note note) {
-        Log.d("NoteRepository", "addNoteToDatabase() method called");
-        databaseHelper.addNote(note);
+        Log.d("NoteRepository", "addNoteToDatabase() method in NoteRepository called");
+        executor.execute(() -> {
+            databaseHelper.addNote(note);
+        });
+    }
+
+    public LiveData<List<Note>> getAllNotes() {
+        MutableLiveData<List<Note>> notes = new MutableLiveData<>();
+        executor.execute(() -> {
+            List<Note> noteList = databaseHelper.getAllNotes();
+            notes.postValue(noteList);
+        });
+        return notes;
     }
 }
