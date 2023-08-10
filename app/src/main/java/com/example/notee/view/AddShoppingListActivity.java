79008309ee29e4a0f85ed6c1a23af1eed71c136a
Lasma Poksane <laaasma.p@gmail.com>
2023-08-10 17:38:08 +0200

    create adding a shopping list functionality (Lasma)

--- app/src/main/java/com/example/notee/view/AddShoppingListActivity.java
+++ app/src/main/java/com/example/notee/view/AddShoppingListActivity.java
@@ -1,12 +1,15 @@
 package com.example.notee.view;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import com.example.notee.R;
 import com.example.notee.ShoppingListDatabase;
+import com.example.notee.model.ShoppingList;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -29,6 +32,23 @@ public class AddShoppingListActivity extends AppCompatActivity {
                 "shopping-list-db"
         ).build();
 
+        addShoppingListButton.setOnClickListener(v -> {
+            String name = addShoppingListName.getText().toString();
+            if (!name.isEmpty()) {
+                ShoppingList shoppingList = new ShoppingList(name);
+                insertShoppingList(shoppingList);
+
+                Toast.makeText(this, "Shopping list added!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+    }
+
+    private void insertShoppingList(ShoppingList shoppingList) {
+        AsyncTask.execute(() -> {
+            shoppingListDatabase.shoppingListDao().insert(shoppingList);
+        });
     }
 
 }
