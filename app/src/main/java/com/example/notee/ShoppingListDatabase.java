d57afb151659a401a00cfe646b0326c0cb1b3d4a
Lasma Poksane <laaasma.p@gmail.com>
2023-08-11 02:36:29 +0200

    create database instance (Lasma)

--- app/src/main/java/com/example/notee/ShoppingListDatabase.java
+++ app/src/main/java/com/example/notee/ShoppingListDatabase.java
@@ -6,7 +6,23 @@ import androidx.room.RoomDatabase;
 import com.example.notee.model.ShoppingList;
 import com.example.notee.model.ShoppingListDao;
 
+import android.content.Context;
+
+import androidx.room.Room;
+
 @Database(entities = {ShoppingList.class}, version = 1)
 public abstract class ShoppingListDatabase extends RoomDatabase {
+    private static ShoppingListDatabase instance;
+
     public abstract ShoppingListDao shoppingListDao();
+
+    public static synchronized ShoppingListDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                            ShoppingListDatabase.class, "shopping-list-db")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
 }
