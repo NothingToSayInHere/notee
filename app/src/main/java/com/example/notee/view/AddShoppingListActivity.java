7dd39d32caf72d3909056109187e170464ffd09d
Lasma Poksane <laaasma.p@gmail.com>
2023-08-11 19:04:32 +0200

    fix adding shopping list functionality (Lasma)

--- app/src/main/java/com/example/notee/view/AddShoppingListActivity.java
+++ app/src/main/java/com/example/notee/view/AddShoppingListActivity.java
@@ -1,54 +1,53 @@
 package com.example.notee.view;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.notee.R;
-import com.example.notee.ShoppingListDatabase;
 import com.example.notee.model.ShoppingList;
+import com.example.notee.viewmodel.ShoppingListViewModel;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class AddShoppingListActivity extends AppCompatActivity {
     private TextInputEditText addShoppingListName;
     private MaterialButton addShoppingListButton;
-    private ShoppingListDatabase shoppingListDatabase;
+    private ShoppingListViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_shopping_list);
 
+        viewModel = new ViewModelProvider(this).get(ShoppingListViewModel.class);
+
         addShoppingListName = findViewById(R.id.add_shopping_list_name);
         addShoppingListButton = findViewById(R.id.add_shopping_list_button);
 
-        shoppingListDatabase = Room.databaseBuilder(
-                getApplicationContext(),
-                ShoppingListDatabase.class,
-                "shopping-list-db"
-        ).build();
-
         addShoppingListButton.setOnClickListener(v -> {
-            String name = addShoppingListName.getText().toString();
-            if (!name.isEmpty()) {
-                ShoppingList shoppingList = new ShoppingList(name);
-                insertShoppingList(shoppingList);
+            String nameString = addShoppingListName.getText().toString().trim();
 
-                Toast.makeText(this, "Shopping list added!", Toast.LENGTH_SHORT).show();
+            if (!nameString.isEmpty()) {
+                ShoppingList shoppingList = new ShoppingList(nameString);
+                viewModel.addShoppingList(shoppingList);
+                addShoppingListName.setText("");
+            } else {
+                Toast.makeText(AddShoppingListActivity.this, "Please enter a name.", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        viewModel.getIsShoppingListAdded().observe(this, isShoppingListAdded -> {
+            if (isShoppingListAdded) {
+                Toast.makeText(AddShoppingListActivity.this, "Shopping list added.", Toast.LENGTH_LONG).show();
+                viewModel.setIsShoppingListAdded(false);
                 finish();
             }
         });
 
     }
-
-    private void insertShoppingList(ShoppingList shoppingList) {
-        AsyncTask.execute(() -> {
-            shoppingListDatabase.shoppingListDao().insert(shoppingList);
-        });
-    }
-
 }
