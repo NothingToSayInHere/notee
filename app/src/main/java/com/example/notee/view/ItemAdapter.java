3d4e0bc2e04f1df4ce9c7484f85cfe276b5100f2
Lasma Poksane <laaasma.p@gmail.com>
2023-08-13 00:30:37 +0200

    add strike-through functionality for items (Lasma)

--- app/src/main/java/com/example/notee/view/ItemAdapter.java
+++ app/src/main/java/com/example/notee/view/ItemAdapter.java
@@ -1,5 +1,6 @@
 package com.example.notee.view;
 
+import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,15 +10,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.notee.R;
 import com.example.notee.model.ShoppingListItem;
+import com.example.notee.viewmodel.ShoppingListViewModel;
 import com.google.android.material.textview.MaterialTextView;
 
 import java.util.List;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
     private List<ShoppingListItem> itemList;
+    private final ShoppingListViewModel viewModel;
 
-    public ItemAdapter(List<ShoppingListItem> itemList) {
+    public ItemAdapter(List<ShoppingListItem> itemList, ShoppingListViewModel viewModel) {
         this.itemList = itemList;
+        this.viewModel = viewModel;
     }
 
     @NonNull
@@ -32,6 +36,19 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         ShoppingListItem item = itemList.get(position);
         holder.itemName.setText(item.getItemName());
+
+        if (item.isCrossedOut()) {
+            holder.itemName.setPaintFlags(holder.itemName.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        } else {
+            holder.itemName.setPaintFlags(holder.itemName.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
+        }
+
+        holder.itemView.setOnClickListener(v -> {
+            item.setIsCrossedOut(!item.isCrossedOut());
+            viewModel.updateItem(item);
+            notifyItemChanged(position);
+        });
+
     }
 
     @Override
