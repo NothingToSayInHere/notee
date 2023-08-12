7ecd25d30f8f4a704d26d360548149c07492cd38
Lasma Poksane <laaasma.p@gmail.com>
2023-08-12 16:35:46 +0200

    Add methods for the shopping list items in DAO (Lasma)

--- app/src/main/java/com/example/notee/model/ShoppingListDao.java
+++ app/src/main/java/com/example/notee/model/ShoppingListDao.java
@@ -5,6 +5,7 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -18,5 +19,18 @@ public interface ShoppingListDao {
 
     @Query("SELECT * FROM shopping_list WHERE userUid = :userUid")
     LiveData<List<ShoppingList>> getFullShoppingList(String userUid);
+
+
+    @Insert
+    void insertItem(ShoppingListItem item);
+
+    @Update
+    void updateItem(ShoppingListItem item);
+
+    @Delete
+    void deleteItem(ShoppingListItem item);
+
+    @Query("SELECT * FROM shopping_list_item WHERE listId = :listId")
+    LiveData<List<ShoppingListItem>> getItemsForShoppingList(int listId);
 
 }
