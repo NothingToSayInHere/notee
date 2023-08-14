d14edcc83cceba61b8b35376c75c5bd83edb7e7c
Lasma Poksane <laaasma.p@gmail.com>
2023-08-14 11:54:04 +0200

    Add and remove Firebase Authentication listeners based on whether logged in (Lasma)

--- app/src/main/java/com/example/notee/view/MainActivity.java
+++ app/src/main/java/com/example/notee/view/MainActivity.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 
 import com.example.notee.R;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +22,32 @@ public class MainActivity extends AppCompatActivity {
         MaterialButton loginButton = findViewById(R.id.login_button);
         MaterialButton registerButton = findViewById(R.id.register_button);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = firebaseAuth -> {
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            if (user != null) {
+                startActivity(new Intent(MainActivity.this, NotesActivity.class));
+                finish();
+            }
+        };
+
         loginButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, LoginActivity.class)));
 
         registerButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, RegisterActivity.class)));
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+
 }
